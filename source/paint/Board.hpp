2c339796f87cc48c538e105008fa06e161b0b66e
Tobi <tobid7@outlook.de>
2021-08-01 13:50:26 +0200

    Create Board.hpp

---
+++ source/paint/Board.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "renderd7.hpp"
+
+namespace Paint
+{
+    class Board
+    {
+        public:
+        Board(int w, int h, bool centered = true);
+        ~Board();
+    }
+}
