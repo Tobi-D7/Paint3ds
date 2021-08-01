48890c413c4ecf0c655521b78b2e40c083bd7ddc
Tobi <tobid7@outlook.de>
2021-08-01 14:42:17 +0200

    Update Board.hpp

--- source/paint/Board.hpp
+++ source/paint/Board.hpp
@@ -8,5 +8,15 @@ namespace Paint
         public:
         Board(int w, int h, bool centered = true);
         ~Board();
-    }
+        private:
+        struct Pixel
+        {
+            int x,
+            iny y,
+            u8 r,
+            u8 g,
+            u8 b
+        };
+        std::vector<Pixel> buard;
+    };
 }
