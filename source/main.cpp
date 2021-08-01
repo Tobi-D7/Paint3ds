b39bb2dc6fb436a3068711ada2391c75a676339d
Tobi <tobid7@outlook.de>
2021-08-01 17:46:20 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,17 @@
+#include "renderd7.hpp"
 
+#include "Board.hpp"
+
+int main()
+{
+    RenderD7::Init::Main();
+    Paint::Board board(48, 48);
+    
+    while(RenderD7::MainLoop())
+    {
+        RenderD7::OnScreen(Top);
+        
+        C3D_FrameEnd(0);
+    }
+    RenderD7::Exit::Main();
+}
