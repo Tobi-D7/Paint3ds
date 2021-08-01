18980ba5fe9d093115b934ce4f1223f3edf23275
Tobi <tobid7@outlook.de>
2021-08-01 16:23:08 +0200

    Create Board.cpp

---
+++ source/paint/Board.cpp
@@ -0,0 +1,14 @@
+#include "Board.hpp"
+
+Paint::Pixel px;
+Paint::Board(int w, int h, bool centered)
+{
+    for (int x = 0; x < w; x++)
+    {
+        for (int y = 0; y < h; y++)
+        {
+            px = {x, y, 255, 255, 255};
+            this->board.push_back()
+        }
+    }
+}
