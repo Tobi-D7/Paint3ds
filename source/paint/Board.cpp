00e6956e39624a86e3f113b8060542e5e8da6492
Tobi <tobid7@outlook.de>
2021-08-06 23:10:26 +0200

    Update Board.cpp

--- source/paint/Board.cpp
+++ source/paint/Board.cpp
@@ -20,6 +20,6 @@ void Paint::Board::Draw()
 {
     for (int i = 0; i < (int)this->board.size(); i++)
     {
-        RenderD7::DrawPx(this->board[i].x(), this->board[i].y(), C2D_Color32(this->board[i].r(), this->board[i].g(), this->board[i].b(), 255));
+        RenderD7::DrawPx(this->board[i].x, this->board[i].y, C2D_Color32(this->board[i].r, this->board[i].g, this->board[i].b, 255));
     }
 }
