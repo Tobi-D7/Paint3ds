44e9879a9ca419c4efe91c57e84eec0a8a6cc5a1
Tobi <tobid7@outlook.de>
2021-08-07 00:52:44 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     RenderD7::Init::Main();
     Paint::Board board(48, 48);
+    std::vector<int> v;
     
     while(RenderD7::MainLoop())
     {
@@ -16,6 +17,7 @@ int main()
         board.Draw();
         RenderD7::OnScreen(Top);
         RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), RenderD7::GetFramerate());
+        RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), std::to_stribg((int)v.max_size());
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
