4b7756438c44679abd0886c1c64734c479e5c892
Tobi <tobid7@outlook.de>
2022-04-17 12:34:26 +0200

    __

--- source/exporter/exporter.cpp
+++ source/exporter/exporter.cpp
@@ -6,13 +6,13 @@
 
 void Export(std::vector<Paint::Pixel> board, int width, int height)
 {
-    const void* buffer = NULL;
+    uint8_t* buffer = new uint8_t[width * height * 4];
     for (size_t i = 0; i < board.size(); i++){
         size_t index = 4 * (board[i].y * width + board[i].x);
-        ((uint8_t*)buffer)[index + 0] = ((uint8_t)board[i].r);
-        ((uint8_t*)buffer)[index + 1] = ((uint8_t)board[i].g);
-        ((uint8_t*)buffer)[index + 2] = ((uint8_t)board[i].b);
-        ((uint8_t*)buffer)[index + 3] = ((uint8_t)255);
+        buffer[index + 0] = ((int)board[i].r);
+        buffer[index + 1] = ((int)board[i].g);
+        buffer[index + 2] = ((int)board[i].b);
+        buffer[index + 3] = ((int)255);
     }
     stbi_write_bmp("sdmc:/file.bmp", width, height, 3, buffer);
 }
