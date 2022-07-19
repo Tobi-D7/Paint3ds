bb4e214f71dd4dc4395e3ddf46752744fa20295b
Tobi <tobid7@outlook.de>
2022-07-19 17:09:41 +0200

    Test

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,7 @@ int main()
     cobj___ = 15500; //3ds cant load more objects
     //Init with patched value. To patch you need to call the cobj___ before init!
     RenderD7::Init::Main("Paint-3ds");
-    Paint::Board board(100, 100);
+    Paint::Board board(500, 500);
     
     std::vector<int> v;
     
@@ -24,7 +24,7 @@ int main()
         if (d7_hDown & KEY_UP) size ++;
         if ((size > 1 && d7_hDown & KEY_DOWN)) size --;
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
-        if (d7_hDown & KEY_SELECT) Export(board.GetBoard(), 100, 100);
+        if (d7_hDown & KEY_SELECT) Export(board.GetBoard(), 500, 500);
         if (d7_hDown & KEY_B) RenderD7::LoadSettings();
         board.SetScale(size);
         hidTouchRead(&t);
@@ -32,11 +32,11 @@ int main()
         RenderD7::OnScreen(Bottom);
         board.Draw();
         RenderD7::OnScreen(Top);
-        RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), RenderD7::GetFramerate());
+        //RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), RenderD7::GetFramerate());
         RenderD7::DrawText(0, 30, 0.7f, C2D_Color32(255, 255, 255, 255), std::to_string(board.D_VectorSize()) + "/" + std::to_string((int)v.max_size()));
-        RenderD7::DrawText(0, 50, 0.7f, C2D_Color32(255, 255, 255, 255), "CPU: " + std::to_string(C3D_GetProcessingTime()*6.0f) + "/" + std::to_string(C3D_GetProcessingTime()));
-        RenderD7::DrawText(0, 70, 0.7f, C2D_Color32(255, 255, 255, 255), "GPU: " + std::to_string(C3D_GetDrawingTime()*6.0f) + "/" + std::to_string(C3D_GetDrawingTime()));
-        RenderD7::DrawText(0, 90, 0.7f, C2D_Color32(255, 255, 255, 255), "CMD: " + std::to_string(C3D_GetCmdBufUsage()*100.0f) + "/" + std::to_string(C3D_GetCmdBufUsage()));
+        //RenderD7::DrawText(0, 50, 0.7f, C2D_Color32(255, 255, 255, 255), "CPU: " + std::to_string(C3D_GetProcessingTime()*6.0f) + "/" + std::to_string(C3D_GetProcessingTime()));
+        //RenderD7::DrawText(0, 70, 0.7f, C2D_Color32(255, 255, 255, 255), "GPU: " + std::to_string(C3D_GetDrawingTime()*6.0f) + "/" + std::to_string(C3D_GetDrawingTime()));
+        //RenderD7::DrawText(0, 90, 0.7f, C2D_Color32(255, 255, 255, 255), "CMD: " + std::to_string(C3D_GetCmdBufUsage()*100.0f) + "/" + std::to_string(C3D_GetCmdBufUsage()));
         
         //RenderD7::DrawMetrikOvl();
         RenderD7::FrameEnd();
